771cd0a5f28afa1e73e1088808b4392481a956c7
SHHSchaeeun <129951743+SHHSchaeeun@users.noreply.github.com>
2023-06-11 19:15:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 from datetime import datetime
 import calendar
 
-data_path = 'bike_sharing_demand/'
+data_path = 'bikeexample/'
 train = pd.read_csv(data_path + 'train.csv', parse_dates=['datetime'])
 test = pd.read_csv(data_path + 'test.csv', parse_dates=['datetime'])
 submission = pd.read_csv(data_path + 'sampleSubmission.csv', parse_dates=['datetime'])
